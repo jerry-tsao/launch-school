ff195cef96b446c68eeac33028ac8388aa4c53ae
Jerry Tsao <jerry.tsao@gmail.com>
2024-09-19 15:30:00 -0700

    Loan Calculator: consolidate repetitive code

--- PY101 - Programming Foundations with Python - Basics/Lesson 2 - Small Programs/loan_calculator.py
+++ PY101 - Programming Foundations with Python - Basics/Lesson 2 - Small Programs/loan_calculator.py
@@ -4,15 +4,31 @@ import math
 import os
 from subprocess import call
 
+info = {
+    'loan_amount': '',
+    'apr': '',
+    'loan_duration_years': '',
+    'loan_duration_months': '',
+    'monthly_payment': '',
+}
+
+def reset_info():
+    info['loan_amount'] = ''
+    info['apr'] = ''
+    info['loan_duration_years'] = ''
+    info['loan_duration_months'] = ''
+    info['monthly_payment'] = ''
+
 def clear():
     _ = call('clear' if os.name == 'posix' else 'cls')
 
-def info_block(loan_amount = '',
-                apr = '',
-                loan_duration_years = '',
-                loan_duration_months = '',
-                monthly_payment = ''):
-    clear()
+def display_info_block():
+    loan_amount = info['loan_amount']
+    apr = info['apr']
+    loan_duration_years = info['loan_duration_years']
+    loan_duration_months = info['loan_duration_months']
+    monthly_payment = info['monthly_payment']
+
     print()
     print('-------------------------------------')
     print()
@@ -31,6 +47,10 @@ def info_block(loan_amount = '',
     print()
     print('-------------------------------------')
     print()
+
+def update_info_block():
+    clear()
+    display_info_block()
 
 def invalid_number(num):
     try:
@@ -76,7 +96,7 @@ def check_y_n():
 
 def get_loan_amount():
     while True:
-        print('What is the loan amount?')
+        print('What is the loan amount in dollars ($)?')
         loan_amount = input('Loan amount: $')
         print()
 
@@ -101,14 +121,14 @@ def get_loan_amount():
 
 def get_apr():
     while True:
-        print('What is the APR in %?')
+        print('What is the APR in percent (%)?')
         apr = input('APR: ')
         print()
 
         while invalid_apr(apr):
             print(f'{apr} is an invalid APR.')
             print()
-            print('Please enter a valid APR of 0% or greater (numbers only).')
+            print('Please enter a valid APR of 0% or greater.')
             apr = input('APR: ')
             print()
 
@@ -128,27 +148,28 @@ def get_loan_duration():
     while True:
         print('Would you like to enter the loan duration in years or months?')
         print('Enter "y" for years or "m" for months.')
-        duration_type = input('Years or months: ').lower()
+        duration_type = input('(y/m): ').lower()
         print()
         while True:
             if duration_type.startswith('y') or duration_type.startswith('m'):
                 break
 
             print('Please enter "y" or "m".')
-            duration_type = input('Years or months: ').lower()
+            duration_type = input('(y/m): ').lower()
             print()
 
         duration_type = duration_type[0]
 
-        print('What is the loan duration?')
-        loan_duration = input('Loan duration: ')
+        print('What is the loan duration in '
+              f'{'years' if duration_type == 'y' else 'months'}?')
+        loan_duration = input('#: ')
         print()
 
         while invalid_number(loan_duration):
             print(f'{loan_duration} is an invalid loan duration.')
             print()
             print('Please enter a valid loan duration greater than 0.')
-            loan_duration = input('Loan duration: ')
+            loan_duration = input('#: ')
             print()
 
         loan_duration_cleaned = loan_duration.replace('$', '').replace(',', '')
@@ -161,7 +182,7 @@ def get_loan_duration():
             float(loan_duration_cleaned) if duration_type == 'm'
             else float(loan_duration_cleaned) * 12)
 
-        print(f'Your loan duration is {loan_duration_years:,.1f} year(s) or '
+        print(f'Your loan duration is {loan_duration_years:,.2f} year(s) or '
             f'{loan_duration_months:,.0f} month(s).')
         print('Is this correct? (y/n)')
 
@@ -187,41 +208,47 @@ def calculate_monthly_payment(loan_amount, apr, loan_duration_months):
     return monthly_payment
 
 def loan_calculator():
-    info_block()
+    clear()
+    display_info_block()
 
     loan_amount = get_loan_amount()
-    info_block(f'${loan_amount:,.2f}')
+    info['loan_amount'] = f'${loan_amount:,.2f}'
+    update_info_block()
 
     apr = get_apr()
-    info_block(f'${loan_amount:,.2f}', f'{apr:,.3f}%')
+    info['apr'] = f'{apr:,.3f}%'
+    update_info_block()
 
     loan_duration_years, loan_duration_months = get_loan_duration()
-    info_block(f'${loan_amount:,.2f}',
-               f'{apr:,.3f}%',
-               f'{loan_duration_years:,.1f}',
-               f'{loan_duration_months:,.0f}')
+    info['loan_duration_years'] = f'{loan_duration_years:,.2f}'
+    info['loan_duration_months'] = f'{loan_duration_months:,.0f}'
+    update_info_block()
 
     monthly_payment = calculate_monthly_payment(loan_amount,
                                                 apr,
                                                 loan_duration_months)
-    info_block(f'${loan_amount:,.2f}',
-               f'{apr:,.3f}%',
-               f'{loan_duration_years:,.1f}',
-               f'{loan_duration_months:,.0f}',
-               f'${monthly_payment:,.2f}')
+    info['monthly_payment'] = f'${monthly_payment:,.2f}'
+    update_info_block()
 
-def main():
+    print('Thank you for using the loan calculator!')
+    print()
+
+def new_calculation():
     while True:
-        loan_calculator()
-
-        print('Thank you for using the loan calculator!')
-        print()
         print('Would you like to calculate another monthly payment? (y/n)')
 
-        ans = check_y_n()
+        answer = check_y_n()
 
-        if ans == 'n':
+        if answer == 'n':
+            clear()
             break
+
+        reset_info()
+        loan_calculator()
+
+def main():
+    loan_calculator()
+    new_calculation()
 
 if __name__ == '__main__':
     main()
